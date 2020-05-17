f137ae270a860de96052bfc7a48cbe1e1911b72b
VictorJardian <victorjardian@ucl.br>
2020-05-17 23:26:33 +0000

    Não deu para fazer do jeito que gostaria realmente de ter feito, e não tem tudo que queria fazer mais está ai.

--- main.cs
+++ main.cs
@@ -68,15 +68,6 @@ public static void verlistaInfFuncionarios(){
      Console.WriteLine (entradaArq);
     }
 }
-
-public void escolherFuncionario(int escfun){
-    
-    
-}
-
-
-
-
 }
 
 class GeradorFicha{
@@ -99,7 +90,6 @@ public GeradorFicha(){
 localConsulta = "Av. Valdopolis, 2000, Telefone: (27) 2000 - 2000, Aberto de Seg. a Sexta das 08:00 as 16:00.";
 nomeEmpresa = "ValdosMetalurgia";
 }
-
 }
 
 class MainClass {
@@ -111,6 +101,7 @@ class MainClass {
     int sen;
     int opcao;
     string[] vet = new string[10];
+    string escolha="nao";
 
     Console.WriteLine ("Bem Vindo ao Programa de CPMSO, Digite seu login e senha.\n");
     Login log = new Login();
@@ -124,206 +115,215 @@ class MainClass {
 
     if(verificacao == true){
 
-      GeradorFicha gf = new GeradorFicha();
+      while(escolha!="sim"){
 
-      Console.WriteLine ("Digite o numero da opção que deseja\n");
+        GeradorFicha gf = new GeradorFicha();
 
-      Interface inter = new Interface();
-      inter.exibirOpcoes();
-      opcao = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine ("Digite o numero da opção que deseja\n");
 
-      if(opcao==1) //Gerar ficha medica da Empresa.
-      {
-        Console.Clear();
+        Interface inter = new Interface();
+        inter.exibirOpcoes();
+        opcao = Convert.ToInt32(Console.ReadLine());
+
+        if(opcao==1) //Gerar ficha medica da Empresa.
+        {
         
-        int escfun;
-        
-        string exameFazer;
+          
+          Console.Clear();
+  
+          int escfun;
+          
+          string exameFazer;
 
-        Console.WriteLine ("Escolha o número do funcionario para Obter os dados.\n");
-        Interface.verlistaInfFuncionarios();
-        Console.WriteLine ("\n");
-        escfun = Convert.ToInt32(Console.ReadLine());
-        Console.Clear();
-        for(int i=0; i<10; i++){
-        vet[i] = lendoFuncionarios.ReadLine();
+          Console.WriteLine ("Escolha o número do funcionario para Obter os dados.\n");
+          Interface.verlistaInfFuncionarios();
+          Console.WriteLine ("\n");
+          escfun = Convert.ToInt32(Console.ReadLine());
+          Console.Clear();
+          for(int i=0; i<10; i++){
+          vet[i] = lendoFuncionarios.ReadLine();
+          }
+          Console.WriteLine ("Digite a letra que representa o motivo da Consulta:\n");
+          Console.WriteLine ("A - Admissional, D - Demissional, P - Periódico, R - Retorno ao Trabalho, M - Mudança de Função.\n");
+          motivo = Convert.ToChar(Console.ReadLine());
+          Console.Clear();
+          switch(motivo){
+            case 'A':
+              motivoString = "Admissional";
+              break;
+            case 'a':
+              motivoString = "Admissional";
+              break;
+            case 'D':
+              motivoString = "Demissional";
+              break;
+            case 'd':
+              motivoString = "Demissional";
+              break;
+            case 'P':
+              motivoString = "Periódico";
+              break;
+            case 'p':
+              motivoString = "Periódico";
+              break;
+            case 'R':
+              motivoString = "Retorno ao Trabalho";
+              break;
+            case 'r':
+              motivoString = "Retorno ao Trabalho";
+              break;
+            case 'M':
+              motivoString = "Mudança de Função";
+              break;
+            case 'm':
+              motivoString = "Mudança de Função";
+              break;
+          }
+          Console.WriteLine ("Digite o exame que deseja mandar o funcionario fazer:\n");
+          Interface.verlistaExames();
+          Console.WriteLine ("\n");
+          exameFazer = Console.ReadLine();
+          Console.WriteLine ("\n");
+          Console.Clear();
+          Console.WriteLine ("Gerando... PDF\n");
+          Console.WriteLine("=====================================================================================================");
+          Console.WriteLine("|Nome          |Nascimento  |RG            |CPF            |Setor         |Função        |Ultimo ASO");
+          Console.WriteLine ("{0}",vet[escfun-1]);
+          Console.WriteLine ("");
+          Console.WriteLine ("*MOTIVO*");
+          Console.WriteLine ("");
+          Console.WriteLine ("{0}",motivoString);
+          Console.WriteLine ("");
+          Console.WriteLine ("*EXAME*");
+          Console.WriteLine ("");
+          Console.WriteLine ("{0}",exameFazer);
+          Console.WriteLine ("");
+          Console.WriteLine("=====================================================================================================");
+          Console.WriteLine ("\n");
         }
-        Console.WriteLine ("Digite a letra que representa o motivo da Consulta:\n");
-        Console.WriteLine ("A - Admissional, D - Demissional, P - Periódico, R - Retorno ao Trabalho, M - Mudança de Função.\n");
-        motivo = Convert.ToChar(Console.ReadLine());
-        Console.Clear();
-        switch(motivo){
-          case 'A':
-            motivoString = "Admissional";
-            break;
-          case 'a':
-            motivoString = "Admissional";
-            break;
-          case 'D':
-            motivoString = "Demissional";
-            break;
-          case 'd':
-            motivoString = "Demissional";
-            break;
-          case 'P':
-            motivoString = "Periódico";
-            break;
-          case 'p':
-            motivoString = "Periódico";
-            break;
-          case 'R':
-            motivoString = "Retorno ao Trabalho";
-            break;
-          case 'r':
-            motivoString = "Retorno ao Trabalho";
-            break;
-          case 'M':
-            motivoString = "Mudança de Função";
-            break;
-          case 'm':
-            motivoString = "Mudança de Função";
-            break;
+
+        if(opcao==2) //Gerar ficha medica da Clinica.
+        {
+
+          Console.Clear();
+          
+          int escfun;
+          
+          string exameFazer;
+
+          Console.WriteLine ("Escolha o número do funcionario para Obter os dados.\n");
+          Interface.verlistaInfFuncionarios();
+          Console.WriteLine ("\n");
+          escfun = Convert.ToInt32(Console.ReadLine());
+          Console.Clear();
+          for(int i=0; i<10; i++){
+          vet[i] = lendoFuncionarios.ReadLine();
+          }
+          Console.WriteLine ("Digite a letra que representa o motivo da Consulta:\n");
+          Console.WriteLine ("A - Admissional, D - Demissional, P - Periódico, R - Retorno ao Trabalho, M - Mudança de Função.\n");
+          motivo = Convert.ToChar(Console.ReadLine());
+          Console.Clear();
+          switch(motivo){
+            case 'A':
+              motivoString = "Admissional";
+              break;
+            case 'a':
+              motivoString = "Admissional";
+              break;
+            case 'D':
+              motivoString = "Demissional";
+              break;
+            case 'd':
+              motivoString = "Demissional";
+              break;
+            case 'P':
+              motivoString = "Periódico";
+              break;
+            case 'p':
+              motivoString = "Periódico";
+              break;
+            case 'R':
+              motivoString = "Retorno ao Trabalho";
+              break;
+            case 'r':
+              motivoString = "Retorno ao Trabalho";
+              break;
+            case 'M':
+              motivoString = "Mudança de Função";
+              break;
+            case 'm':
+              motivoString = "Mudança de Função";
+              break;
+          }
+          Console.WriteLine ("Digite o exame que deseja mandar o funcionario fazer:\n");
+          Interface.verlistaExames();
+          Console.WriteLine ("\n");
+          exameFazer = Console.ReadLine();
+          Console.WriteLine ("\n");
+          Console.Clear();
+          Console.WriteLine ("Gerando... PDF\n");
+          Console.WriteLine("=====================================================================================================");
+          Console.WriteLine("|Nome          |Nascimento  |RG            |CPF            |Setor         |Função        |Ultimo ASO");
+          Console.WriteLine ("{0}",vet[escfun-1]);
+          Console.WriteLine ("");
+          Console.WriteLine ("*MOTIVO*");
+          Console.WriteLine ("");
+          Console.WriteLine ("{0}",motivoString);
+          Console.WriteLine ("");
+          Console.WriteLine ("*EXAME*");
+          Console.WriteLine ("");
+          Console.WriteLine ("{0}",exameFazer);
+          Console.WriteLine ("");
+          Console.WriteLine ("*EMPRESA: {0}*",gf.GetnomeEmpresa());
+          Console.WriteLine ("");
+          Console.WriteLine ("O local e horario de funcionamento da clinica são:");
+          Console.WriteLine (gf.GetlocalConsulta());
+          Console.WriteLine ("");
+          Console.WriteLine("=====================================================================================================");
+          Console.WriteLine ("\n");
         }
-        Console.WriteLine ("Digite o exame que deseja mandar o funcionario fazer:\n");
-        Interface.verlistaExames();
-        Console.WriteLine ("\n");
-        exameFazer = Console.ReadLine();
-        Console.WriteLine ("\n");
-        Console.Clear();
-        Console.WriteLine ("Gerando... PDF\n");
-        Console.WriteLine("=====================================================================================================");
-        Console.WriteLine("|Nome          |Nascimento  |RG            |CPF            |Setor         |Função        |Ultimo ASO");
-        Console.WriteLine ("{0}",vet[escfun-1]);
-        Console.WriteLine ("");
-        Console.WriteLine ("*MOTIVO*");
-        Console.WriteLine ("");
-        Console.WriteLine ("{0}",motivoString);
-        Console.WriteLine ("");
-        Console.WriteLine ("*EXAME*");
-        Console.WriteLine ("");
-        Console.WriteLine ("{0}",exameFazer);
-        Console.WriteLine ("");
-        Console.WriteLine("=====================================================================================================");
-    
 
-
-
-      }
-
-      if(opcao==2) //Gerar ficha medica da Clinica.
-      {
-
-        Console.Clear();
-        
-        int escfun;
-        
-        string exameFazer;
-
-        Console.WriteLine ("Escolha o número do funcionario para Obter os dados.\n");
-        Interface.verlistaInfFuncionarios();
-        Console.WriteLine ("\n");
-        escfun = Convert.ToInt32(Console.ReadLine());
-        Console.Clear();
-        for(int i=0; i<10; i++){
-        vet[i] = lendoFuncionarios.ReadLine();
+        if(opcao==3) //Visualizar lista de funcionarios.
+        {
+          Console.Clear();
+          Interface.verlistaInfFuncionarios();
         }
-        Console.WriteLine ("Digite a letra que representa o motivo da Consulta:\n");
-        Console.WriteLine ("A - Admissional, D - Demissional, P - Periódico, R - Retorno ao Trabalho, M - Mudança de Função.\n");
-        motivo = Convert.ToChar(Console.ReadLine());
-        Console.Clear();
-        switch(motivo){
-          case 'A':
-            motivoString = "Admissional";
-            break;
-          case 'a':
-            motivoString = "Admissional";
-            break;
-          case 'D':
-            motivoString = "Demissional";
-            break;
-          case 'd':
-            motivoString = "Demissional";
-            break;
-          case 'P':
-            motivoString = "Periódico";
-            break;
-          case 'p':
-            motivoString = "Periódico";
-            break;
-          case 'R':
-            motivoString = "Retorno ao Trabalho";
-            break;
-          case 'r':
-            motivoString = "Retorno ao Trabalho";
-            break;
-          case 'M':
-            motivoString = "Mudança de Função";
-            break;
-          case 'm':
-            motivoString = "Mudança de Função";
-            break;
+
+        if(opcao==4) //Trocar local e horario de funcionamento da clinica..
+        { 
+          Console.Clear();
+          Console.WriteLine ("O novo local e horario de funcionamento da clinica é:");
+          gf.SetlocalConsulta(Console.ReadLine());
+          Console.WriteLine ("\n");
+          Console.WriteLine ("Você trocou o local e horario para:\n");
+          Console.WriteLine (gf.GetlocalConsulta());
+          Console.WriteLine ("\n");
         }
-        Console.WriteLine ("Digite o exame que deseja mandar o funcionario fazer:\n");
-        Interface.verlistaExames();
-        Console.WriteLine ("\n");
-        exameFazer = Console.ReadLine();
-        Console.WriteLine ("\n");
-        Console.Clear();
-        Console.WriteLine ("Gerando... PDF\n");
-        Console.WriteLine("=====================================================================================================");
-        Console.WriteLine("|Nome          |Nascimento  |RG            |CPF            |Setor         |Função        |Ultimo ASO");
-        Console.WriteLine ("{0}",vet[escfun-1]);
-        Console.WriteLine ("");
-        Console.WriteLine ("*MOTIVO*");
-        Console.WriteLine ("");
-        Console.WriteLine ("{0}",motivoString);
-        Console.WriteLine ("");
-        Console.WriteLine ("*EXAME*");
-        Console.WriteLine ("");
-        Console.WriteLine ("{0}",exameFazer);
-        Console.WriteLine ("");
-        Console.WriteLine ("*EMPRESA: {0}*",gf.GetnomeEmpresa());
-        Console.WriteLine ("");
-        Console.WriteLine ("O local e horario de funcionamento da clinica são:");
-        Console.WriteLine (gf.GetlocalConsulta());
-        Console.WriteLine ("");
-        Console.WriteLine("=====================================================================================================");
-      }
 
-      if(opcao==3) //Visualizar lista de funcionarios.
-      {
-        Console.Clear();
-        Interface.verlistaInfFuncionarios();
-      }
+        if(opcao==5) //Visualizar lista de Exames.
+        { 
+          Console.Clear();
+          Interface.verlistaExames();
+          Console.WriteLine ("\n");
+        }
 
-      if(opcao==4) //Trocar local e horario de funcionamento da clinica..
-      { 
-        Console.Clear();
-        Console.WriteLine ("O novo local e horario de funcionamento da clinica é:");
-        gf.SetlocalConsulta(Console.ReadLine());
-        Console.WriteLine ("\n");
-        Console.WriteLine ("Você trocou o local e horario para:\n");
-        Console.WriteLine (gf.GetlocalConsulta());
-      }
-
-      if(opcao==5) //Visualizar lista de Exames.
-      { 
-        Console.Clear();
-        Interface.verlistaExames();
-      }
-
-      if(opcao==6) //Visualizar lista de Funções.
-      { 
-        Console.Clear();
-        Interface.verlistaFuncoes();
-      }
+        if(opcao==6) //Visualizar lista de Funções.
+        { 
+          Console.Clear();
+          Interface.verlistaFuncoes();
+          Console.WriteLine ("\n");
+        }
 
 
-      if(opcao==7) //SAIR.
-      { 
+        if(opcao==7) //SAIR.
+        { 
+          Console.Clear();
+          Console.WriteLine("Tem certeza que:");
+        }
+        Console.WriteLine("Deseja sair?");
+        escolha = Console.ReadLine();
         Console.Clear();
       }
-
     }
   }
 }
